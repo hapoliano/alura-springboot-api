9a637cb7ac325c3dc67825371b54ecf40650a693
Daniel Apoliano <danielapoliano02@gmail.com>
2025-05-25 09:56:15 -0300

    Lidando com erros

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,6 @@
 package med.voll.api.controller;
 
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
