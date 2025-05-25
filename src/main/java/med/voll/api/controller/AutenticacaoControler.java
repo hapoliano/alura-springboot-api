5b22f2d7aeb4d56895642f5e0869972126f8b69b
Daniel Apoliano <danielapoliano02@gmail.com>
2025-05-25 18:25:34 -0300

    JSON Web Token

--- src/main/java/med/voll/api/controller/AutenticacaoControler.java
+++ src/main/java/med/voll/api/controller/AutenticacaoControler.java
@@ -1,6 +1,9 @@
 package med.voll.api.controller;
 
 import med.voll.api.domain.usuario.DaddosAutenticacao;
+import med.voll.api.domain.usuario.Usuario;
+import med.voll.api.infra.security.DadosTokenJWT;
+import med.voll.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -17,11 +20,16 @@ public class AutenticacaoControler {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody DaddosAutenticacao dados) {
-        var token = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-        var authentication = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+        var authentication = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
